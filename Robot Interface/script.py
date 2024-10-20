a6ef5b56eaf14c2c9c8465506836b2ecec0adced
Greg Conroy <greg.conroy.mail@gmail.com>
2024-10-20 18:34:51 +0200

    updated led code

--- Robot Interface/script.py
+++ Robot Interface/script.py
@@ -7,26 +7,30 @@ motor_pin_b = 23  # BOARD pin 16, BCM pin 16
 
 def main():
     # Pin Setup:
-    # Board pin-numbering scheme
     GPIO.setmode(GPIO.BCM)
-    # Set both pins LOW to keep the motor idle
-    # You can keep one of them HIGH and the LOW to start with rotation in one direction 
     GPIO.setup(motor_pin_a, GPIO.OUT, initial=GPIO.LOW)
     GPIO.setup(motor_pin_b, GPIO.OUT, initial=GPIO.LOW)
 
     print("Starting demo now! Press CTRL+C to exit")
+    
+    # Start with motor_pin_a HIGH and motor_pin_b LOW
     curr_value_pin_a = GPIO.HIGH
     curr_value_pin_b = GPIO.LOW
+    
     try:
         while True:
-            time.sleep(5)
-            # Toggle the output every second
-            print("Outputting {} to pin {} AND {} to pin {}".format(curr_value_pin_a,        motor_pin_a, curr_value_pin_b, motor_pin_b))
+            # Toggle the output every half a second (0.5 seconds)
+            time.sleep(0.5)
+            print("Outputting {} to pin {} AND {} to pin {}".format(curr_value_pin_a, motor_pin_a, curr_value_pin_b, motor_pin_b))
             GPIO.output(motor_pin_a, curr_value_pin_a)
             GPIO.output(motor_pin_b, curr_value_pin_b)
-            curr_value_pin_a ^= GPIO.HIGH
-            curr_value_pin_a ^= GPIO.LOW
+            
+            # Toggle the pin values
+            curr_value_pin_a = GPIO.LOW if curr_value_pin_a == GPIO.HIGH else GPIO.HIGH
+            curr_value_pin_b = GPIO.LOW if curr_value_pin_b == GPIO.HIGH else GPIO.HIGH
+            
     finally:
         GPIO.cleanup()
 
-main()
+if __name__ == "__main__":
+    main()
